45606f92fd1e71ffe8998c6db84d041e8c3c0694
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-16 16:17:26 +0100

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container py-4">
+      <h1 className="mb-4">Crea un nuovo Post</h1>
+
+      <form className="card p-4 shadow-sm">
+
+        <div className="mb-3">
+          <label className="form-label">Author</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Body</label>
+          <textarea className="form-control" rows="4"></textarea>
+        </div>
+
+        <div className="form-check mb-3">
+          <input type="checkbox" className="form-check-input" id="publicCheck" />
+          <label className="form-check-label" htmlFor="publicCheck">
+            Public
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Invia Post
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      </form>
+    </div>
   )
 }
-
-export default App

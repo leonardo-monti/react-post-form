6d5c94990760cf5f22350936a73c6099eabae611
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-16 17:20:39 +0100

    add hanlde form change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
+const initialFormData = {
+  author: "",
+  title: "",
+  body: "",
+  public: false
+}
+
 export default function App() {
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleFormChange = (e) => {
+    const { name, value, type, checked } = e.target
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value
+    }))
+  }
+
+
+
   return (
     <div className="container py-4">
       <h1 className="mb-4">Crea un nuovo Post</h1>
@@ -9,22 +30,49 @@ export default function App() {
       <form className="card p-4 shadow-sm">
 
         <div className="mb-3">
-          <label className="form-label">Author</label>
-          <input type="text" className="form-control" />
+          <label className="form-label"
+            htmlFor="authorInput">Author</label>
+          <input
+            id="authorInput"
+            type="text"
+            name="author"
+            value={formData.author}
+            onChange={handleFormChange}
+            className="form-control" />
         </div>
 
         <div className="mb-3">
-          <label className="form-label">Title</label>
-          <input type="text" className="form-control" />
+          <label className="form-label"
+            htmlFor="titleInput">Title</label>
+          <input
+            id="titleInput"
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleFormChange}
+            className="form-control" />
         </div>
 
         <div className="mb-3">
-          <label className="form-label">Body</label>
-          <textarea className="form-control" rows="4"></textarea>
+          <label className="form-label"
+            htmlFor="bodyInput">Body</label>
+          <textarea
+            id="bodyInput"
+            name="body"
+            value={formData.body}
+            onChange={handleFormChange}
+            className="form-control"
+            rows="4"></textarea>
         </div>
 
         <div className="form-check mb-3">
-          <input type="checkbox" className="form-check-input" id="publicCheck" />
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="publicCheck"
+            name="public"
+            checked={formData.public}
+            onChange={handleFormChange} />
           <label className="form-check-label" htmlFor="publicCheck">
             Public
           </label>
